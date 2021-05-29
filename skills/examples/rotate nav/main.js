7a71bdf89cfc7e6b83f2863598c4bcb359bc9f0d
unknown <aboyousef555eng@gmail.com>
2021-05-30 00:13:19 +0200

    rotate nav with faqr

--- skills/examples/rotate nav/main.js
+++ skills/examples/rotate nav/main.js
@@ -1,2 +1,12 @@
 // variables
+const template = document.querySelector('.template')
+const openBtn = document.getElementById('open')
+const closeBtn = document.getElementById('close')
+
+openBtn.addEventListener('click',function(){
+    template.classList.add('showNav')
+})
+closeBtn.addEventListener('click',function(){
+    template.classList.remove('showNav')
+})
 
