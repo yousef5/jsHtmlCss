fadfc0c710bf16741510f4a1e05e34c627cfb610
unknown <aboyousef555eng@gmail.com>
2021-06-08 16:55:18 +0200

    range slider

--- skills/examples/random slider/main.js
+++ skills/examples/random slider/main.js
@@ -1,2 +1,24 @@
 // variables
 
+const slider = document.querySelector(".slider");
+const label = document.querySelector(".label");
+
+slider.addEventListener("input", function (e) {
+  const value = e.target.value;
+  const sliderWidth = getComputedStyle(e.target).getPropertyValue("width");
+  const labelWidth = getComputedStyle(label).getPropertyValue("width");
+  const labelWidthNumber = +labelWidth.substring(0, labelWidth.length - 2);
+  const sliderWidthNumber = +sliderWidth.substring(0, sliderWidth.length - 2);
+  const min = +e.target.min;
+  const max = +e.target.max;
+  const left = value * (sliderWidthNumber / max) - (labelWidthNumber / 2) + scale(value, min, max, 10, -10);
+  console.log(left);
+  console.log(sliderWidthNumber);
+  label.style.left = `${left}px`;
+  label.innerHTML = value;
+});
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  }
